082a576b68c3e7f9deee1b90edf3bb0712aeadb4
mkaryadi <matthew.t.karyadi@gmail.com>
2023-05-02 15:03:32 -0700

    Updated open source date

--- src/widget/Footer.js
+++ src/widget/Footer.js
@@ -6,7 +6,7 @@ export function Footer() {
       <p><strong>Hobbio</strong></p>
       <p><a href="https://www.youtube.com/watch?v=OG_e8ve0l_w">Final Presentation</a> · <a href="https://www.youtube.com/watch?v=OG_e8ve0l_w"> Miro Board</a> · <a href="https://www.youtube.com/watch?v=OG_e8ve0l_w">Hand-off Documents</a></p>
       <p>&copy; Hobbio 2023 University Of Washington</p>
-      <p>Note: this project will be going open source as of June 11, 2023</p>
+      <p>Note: this project will be going open source as of June 4, 2023</p>
     </footer>
   )
 }
